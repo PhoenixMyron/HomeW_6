278c8d7c57cdaedeaf78dd829421af9942c0af51
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-14 00:28:25 +0600

    extra is in procces

---
+++ extra_1/Program.cs
@@ -0,0 +1,36 @@
+﻿void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine("Вывод массива:");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"[{string.Join(", ", array)}]");
+    }
+}
+int MoveArray(int[] array)
+{
+    k = k % n;
+    if (k > 0)
+    {
+    for (int i = 0; i < k; i++)
+    arrayHelp[i] = array[array.Length - k + i];
+    for (int i = 0; i < array.Length - k; i++)
+    arrayHelp[k + i] = array[i];
+
+        Console.WriteLine($"[{string.Join(", ", arrayHelp)}]");
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массивва: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int[] arrayHelp = new int[n];
+int k = Convert.ToInt32(Console.ReadLine());
+
