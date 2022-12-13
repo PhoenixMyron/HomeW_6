92ae705c3f69039a9d4b4c442c7d4280db89130b
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-13 22:56:09 +0600

    task 41 is done

---
+++ task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+void PositiveCount(int count,int[] array)
+{
+    
+    for ( int i = 0; i <= array.Length - 1; i++)
+    {
+        if (array[i] > 0 ) 
+        {
+            count++;
+        }
+    }
+     Console.WriteLine($"{count} \t");
+}
+
+Console.Clear();
+Console.Write("Введите числа: ");
+string[] numbers = Console.ReadLine().Split(" "); 
+int[] array = numbers.Select(int.Parse).ToArray();
+Console.WriteLine(String.Join(", ", array));
+int count = 0;
+PositiveCount(count,array);
